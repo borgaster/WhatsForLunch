3cfa1911fafb37ec86a60935def530f78dc67c70
André Bastos <andrebastos@MacBook-Pro.local>
2016-07-09 23:39:37 +0100

    Rate action moved to Meteor methods

--- imports/api/menu.js
+++ imports/api/menu.js
@@ -5,4 +5,16 @@ Date.prototype.getDayOfWeek = function(){
 }
 export const Menu = new Mongo.Collection('menu');
 
+Meteor.methods({
+	'rateit'(mealCourse, rate,  recordID, description){
+		let menu = Menu.findOne({_id: recordID})
+		menu[mealCourse].forEach((meal) => {
+			if(meal["text"] === description){
+				meal["rating"] = meal["rating"] + rate
+			}
+		})
+		delete menu._id
+		Menu.update({_id:recordID}, { $set: menu });
+	}
+});
 
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Menu } from '../api/menu.js';
+import { Meteor } from 'meteor/meteor';
 import './body.html';
 let dayOfWeek = new Date().getDayOfWeek();
 let menuOfToday = Menu.find({dayOfWeek: "Monday"})
@@ -18,67 +19,30 @@ Template.body.helpers({
 
 Template.soup.events({
 	'click .rateit'(event, template){
-		event.preventDefault();
-		let text = this.text;
-		let menu = Menu.findOne({_id: recordID})
-		console.log(menu)
-		menu["soups"].forEach((soup) => {
-			if(soup["text"] === text){
-				soup["rating"] = soup["rating"] + template.$('.rateit').rateit('value');
-				template.$('.rateit').attr("disabled", true);
-				Menu.update({_id:recordID}, {
-					$set:{
-						soups: menu["soups"] 
-					}
-				});
-			}
-		});
+		let description = this.text;
+		let rate = template.$('.rateit').rateit('value');
+		Meteor.call('rateit', "soups", rate, recordID, description);
 		return false;
 	}
 });
 
 Template.main.events({
 	'click .rateit'(event, template){
-		event.preventDefault();
-		let text = this.text;
-		let menu = Menu.findOne({_id: recordID})
-		console.log(menu)
-		menu["mains"].forEach((mains) => {
-			if(mains["text"] === text){
-				mains["rating"] = mains["rating"] + template.$('.rateit').rateit('value');
-				template.$('.rateit').attr("disabled", true);
-				Menu.update({_id:recordID}, {
-					$set:{
-						mains: menu["mains"] 
-					}
-				});
-			}
-		});
+		let description = this.text;
+		let rate = template.$('.rateit').rateit('value');
+		Meteor.call('rateit', "mains", rate, recordID, description);
 		return false;
 	}
 });
 
 Template.dessert.events({
 	'click .rateit'(event, template){
-		event.preventDefault();
-		let text = this.text;
-		let menu = Menu.findOne({_id: recordID})
-		console.log(menu)
-		menu["desserts"].forEach((desserts) => {
-			if(mains["text"] === text){
-				desserts["rating"] = desserts["rating"] + template.$('.rateit').rateit('value');
-				template.$('.rateit').attr("disabled", true);
-				Menu.update({_id:recordID}, {
-					$set:{
-						desserts: menu["desserts"] 
-					}
-				});
-			}
-		});
+		let description = this.text;
+		let rate = template.$('.rateit').rateit('value');
+		Meteor.call('rateit', "desserts", rate, recordID, description);
 		return false;
 	}
 });
-
 
 // .rateit elements need to be progressively enhanced after they're created
 Template.rating.rendered = function () {
